76ef60c046a41633a4542a9bae12232bf6ee2851
kerryo17 <32079576+kerryo17@users.noreply.github.com>
2019-11-09 11:23:59 -0500

    Add files via upload

---
+++ Day0/Day0/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(int argc, char* argv[]) {
+	
+	for (int i = 0; i < 100; i++) {
+		printf("Hello %d class!\n", "This is a thing");
+		printf("\n");
+	}
+
+	return 0;
+}
